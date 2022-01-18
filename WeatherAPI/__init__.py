eae2292131bb258955e368308e2c8a79703b664e
supportvector <supportvector@protonmail.com>
2022-01-18 18:33:08 +0200

    exception handling

--- WeatherAPI/__init__.py
+++ WeatherAPI/__init__.py
@@ -1,9 +1,5 @@
 import logging
-import json
-import datetime
-import requests
 import re
-import os
 
 import azure.functions as func
 from azure.data.tables import TableServiceClient
@@ -12,22 +8,34 @@ from azure.identity import DefaultAzureCredential
 
 
 def main(req: func.HttpRequest, temperaturesTable) -> func.HttpResponse:
+    """Functions takes date (YYYY-MM-DD) and time (HH:MM:SS) as input and returns
+    the recorded temperature from Turku at the closest recorded full hour of the input time (UTC)
 
-    # KV
-    kv = SecretClient(vault_url="https://mle-weatherapp-kv.vault.azure.net/", credential=DefaultAzureCredential())
-    storage_secret = kv.get_secret("storage-sas-token")
+    ***********
+    * EXAMPLE *
+    ***********
 
+    import requests
+    
+    # Replace FUNCTION_ENDPOINT with whatever endpoint the function is assigned to
+    response = requests.get(FUNCTION_ENDPOINT, params={"date": "2022-01-17", "time": "10:00:00"})
+    print(response.json())
 
-    # Get connection to the table
-    table_service = TableServiceClient(endpoint=storage_secret.value)
-    table = table_service.get_table_client("Temperatures")
+    >> -2.03
+    """
 
-    def valid_input_parameters(date: str, time: str) -> bool:
-        """Light regex to check if input parameters are correctly formatted"""
-        # TODO: Handle day, month order
+    def valid_input_parameters(params: dict) -> bool:
+        """Checks that valid parameters are given and that they are of the correct format"""
+        
+        # Are parameters given and are date and time included
+        if not params:
+            return False
+        elif "date" not in params.keys() or "time" not in params.keys():
+            return False
+        # If parameters provided, are they of correct form
         re_date = r'\d{4}-\d{2}-\d{2}$'
         re_time = r'\d{2}:\d{2}:\d{2}$'
-        if re.match(re_date, date) and re.match(re_time, time):
+        if re.match(re_date, params.get('date')) and re.match(re_time, params.get('time')):
             return True
         else:
             return False
@@ -41,32 +49,41 @@ def main(req: func.HttpRequest, temperaturesTable) -> func.HttpResponse:
         # choose the closest hour based on user input on timestmap
         # e.g. if input time is 15:31:00, return temperature at 16:00:00, if input minute < 30 return temperature at the started hour -> this case 15:00:00
         # TODO: Handle case where the hour shift pushes it into a future hour
-        # TODO: rebuild this entire module
-
         hour = int(hour) + 1 if int(minute) > 30 else hour
         qry_string = f"year eq {year} and month eq {month} and day eq {day} and hour eq {hour} and minute eq 0"
-        res = table.query_entities(qry_string)
+        try:
+            res = table.query_entities(qry_string)
+        except Exception as e:
+            logging.error(f"Error retrieving temperature from table\nError Message: {str(e)}")
+            res = []
 
-        # This should already return only one row, but since during testing the function might be run manually
+        # During testing the function might be run manually
         # It could mean that it returns two valid rows. Pick the latest
         rows = [row for row in res]
         return rows[0].get("Temperature") if rows else None
 
-    # Check if input parameters were given
-    if 'date' not in req.params.keys() or 'time' not in req.params.keys():
-        return func.HttpResponse("Please provide date and time", status_code=400)
+    # get keys and connect to table
+    try:
+        kv = SecretClient(vault_url="https://mle-weatherapp-kv.vault.azure.net/", credential=DefaultAzureCredential())
+        storage_secret = kv.get_secret("storage-sas-token")
+    except Exception as e:
+        logging.error(f"Error while trying to connect to KeyVault\nError msg: {str(e)}")
+        func.HttpResponse("Internal Error", status_code=500)
+    try:
+        table_service = TableServiceClient(endpoint=storage_secret.value)
+        table = table_service.get_table_client("Temperatures")
+    except Exception as e:
+        logging.error(f"Error while trying to connect to table store\nError msg: {str(e)}")
+        func.HttpResponse("Internal Error", status_code=500)
 
+    # Check for valid input
+    if not valid_input_parameters(req.params):
+        return func.HttpResponse("Invalid input parameters, please provide date and time. Date is expected to be 'YYYY-MM-DD' and Time 'HH:MM:SS'", status_code=400)
+    
     date = req.params.get('date')
     time = req.params.get('time')
-
-    if not valid_input_parameters(date, time):
-        return func.HttpResponse("Invalid parameter format. Date is expected to be 'YYYY-MM-DD' and Time 'HH:MM:SS'", status_code=400)
-
     temperature = get_temperature(date, time)
+
     if not temperature:
         return func.HttpResponse("No temperature found for date and time", status_code=404)
-
-    #return func.HttpResponse(\
-    #    f"Date: {date}\nTime: {time}\nTemperature: NA\n\nCURRENT TABLE DATA\n{[row for row in table]}"
-    #    )
-    return func.HttpResponse(str(temperature))
+    return func.HttpResponse(str(temperature), status_code=200)
--- WeatherPoll/__init__.py
+++ WeatherPoll/__init__.py
@@ -5,6 +5,9 @@ import datetime
 import json
 import uuid
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from zoneinfo import ZoneInfo
 
 import azure.functions as func
 from azure.keyvault.secrets import SecretClient
@@ -12,43 +15,54 @@ from azure.identity import DefaultAzureCredential
 
 
 def main(mytimer: func.TimerRequest, temperaturesTable: func.Out[str]) -> None:
-    """ Function polls api.openweathermap.org every hour and stores the temperature in Turku in UTC to a Azure table storage"""
+    """ Function polls api.openweathermap.org every hour and stores the temperature in Turku in local time to a Azure table storage"""
 
-    # TODO: Store as environment variables
-    kv = SecretClient(vault_url="https://mle-weatherapp-kv.vault.azure.net/", credential=DefaultAzureCredential())
-    api_secret = kv.get_secret("weather-api-key")
+    try:
+        kv = SecretClient(vault_url="https://mle-weatherapp-kv.vault.azure.net/", credential=DefaultAzureCredential())
+        api_secret = kv.get_secret("weather-api-key")
+    except Exception as e:
+        logging.error(f"Error while trying to connect to KeyVault\nError msg: {str(e)}")
 
     def get_temperature(city: str="Turku") -> str:
-        """Gets the current weather from openweathermap.org
+        """Gets the current weather from openweathermap.org in provided city
         """
         endpoint = "https://api.openweathermap.org/data/2.5/weather"
         payload = {"q": city, "appid": api_secret.value, 'units': 'metric'}
-        response = requests.get(endpoint, params=payload) # add timeout
+
+        retry_strategy = Retry(
+            total=3,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504]
+            )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        try:
+            response = http.get(endpoint, params=payload)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error while calling openweathermap. Error msg: {str(e)}")
         # TODO: Implement retry and failure logic
         return response.json()['main']['temp']
 
-
-    temperature = get_temperature() # OK
+    temperature = get_temperature()
     # TODO: check if respose provides timestamp of time of measurement and use that instead here
-    dt_utc = datetime.datetime.now().replace(microsecond=0) # object
-    str_timestamp = dt_utc.isoformat(timespec='seconds') # as string
+    # TODO: Use unix timestamp
+    dt_local = datetime.datetime.now(tz=ZoneInfo("Europe/Helsinki")).replace(microsecond=0)
     rowKey = str(uuid.uuid4())
-
     data = {
         "partitionKey": "Turku",
         "rowKey": rowKey,
-        "year": dt_utc.year,
-        "month": dt_utc.month,
-        "day": dt_utc.day,
-        "hour": dt_utc.hour,
-        "minute": dt_utc.minute,
-        "second": dt_utc.second,
+        "year": dt_local.year,
+        "month": dt_local.month,
+        "day": dt_local.day,
+        "hour": dt_local.hour,
+        "minute": dt_local.minute,
+        "second": dt_local.second,
         "Temperature" : temperature
     }
-
-    logging.info(f"DATA: {data}")
-
-    temperaturesTable.set(json.dumps(data))
-
-    if mytimer.past_due:
-        logging.info('Data Read')
+    try:
+        temperaturesTable.set(json.dumps(data))
+        logging.info(f"Successfully read temperature into table store at {str(dt_local)}")
+    except Exception as e:
+        logging.error(f"Error reding data into table store: {str(e)}")
